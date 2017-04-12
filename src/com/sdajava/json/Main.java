128bbb19090c26a47bd16cb3494b802513215372
lukaszog <lukasz.ogan@gmail.com>
2017-04-12 19:01:19 +0200

    add dep

--- src/com/sdajava/json/Main.java
+++ src/com/sdajava/json/Main.java
@@ -1,10 +1,12 @@
 package com.sdajava.json;
 
+import jdk.nashorn.internal.parser.JSONParser;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        JSONParser parser = new JSONParser();
 
 
 
