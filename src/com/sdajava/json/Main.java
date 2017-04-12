ba6fd012d3a753a389e45c3134cad664eb91d0f0
lukaszog <lukasz.ogan@gmail.com>
2017-04-12 19:12:17 +0200

    add dep

--- src/com/sdajava/json/Main.java
+++ src/com/sdajava/json/Main.java
@@ -1,6 +1,14 @@
 package com.sdajava.json;
 
-import jdk.nashorn.internal.parser.JSONParser;
+
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
 
@@ -8,6 +16,18 @@ public class Main {
 
         JSONParser parser = new JSONParser();
 
+        try{
+            URL url = new URL("http://lukaszogan.com/data.json");
+            URLConnection connect = url.openConnection();
+            BufferedReader in =
+                new BufferedReader(new InputStreamReader(connect.getInputStream()));
+
+            in.lines().forEach(System.out::println);
+
+
+        }catch (IOException err){
+
+        }
 
 
     }
